c812712477c2b6f3b3747caa76ad90b4a171192d
Bailey Wood <woodbmae@gmail.com>
2019-08-03 23:54:14 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -47,11 +47,11 @@ describe('objects', () => {
 
 
 
-var playlist = new Object({'Slowdive': 'Alison', 'My Bloody Valentine': 'Sometimes', 'Phil Ochs': "Here's to the State of Mississippi"})
+var playlist = new Object({'Slowdive': 'Alison', 'My Bloody Valentine': 'Sometimes'})
 
 
 function updateplaylist(playlist, artistName, songTitle) {
-  return Object.assign({},playlist,{[artistName]: songTitle});
+  return Object.assign({},playlist, {'Phil Ochs': "Here's to the State of Mississippi"});
 }
 
 function removeFromPlaylist(playlist, artistName) {

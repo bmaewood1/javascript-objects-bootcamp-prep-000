9b37353bcedeabc35ef168311d7f7f24fcec1838
Bailey Wood <woodbmae@gmail.com>
2019-08-04 00:39:24 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -14,3 +14,21 @@ function removeFromPlaylist(playlist, artistName) {
 }
 
 
+var playlist = new Object ({'Slowdive': "Alison", 'My Bloody Valentine': "Sometimes"})
+
+var obj = { prop: 1 }
+function updateObjectWithKeyAndValue(object, key, value) {
+  return Object.assign({},obj,{[key]:value})
+}
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  object[key] = value
+  return object
+}
+function deleteFromObjectByKey(object, key) {
+  delete object.key;
+  return obj
+}
+function destructivelyDeleteFromObjectByKey(object, key) {
+  delete object.key
+  return object
+}

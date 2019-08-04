b3a0678d99fb917e3cbf98844b362d3faa7249cc
Bailey Wood <woodbmae@gmail.com>
2019-08-04 00:30:55 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -4,10 +4,10 @@ var playlist = { 'Slowdive': 'Alison', 'My Bloody Valentine': 'Sometimes' };
 
 
 function updateplaylist(playlist, artistName, songTitle) {
-  playlist[artistName] = songTitle;
+  playlist['Phil Ochs'] = 'Here`s to the State of Mississippi';
 }
 
-updateplaylist(playlist, 'Phil Ochs', 'Here`s to the State of Mississippi');
+
 
 function removeFromPlaylist(playlist, artistName) {
   delete playlist.artistName;

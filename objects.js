937d0ce937478eefa379f75df4d9a60bdb214b3a
Bailey Wood <woodbmae@gmail.com>
2019-08-04 00:11:55 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -48,11 +48,11 @@ describe('objects', () => {
 }
  
 
-
 function updateplaylist(playlist, artistName, songTitle) {
   return Object.assign({}, playlist, {'Phil Ochs': "Here's to the State of Mississippi"});
 }
 
 function removeFromPlaylist(playlist, artistName) {
   delete playlist.artistName;
+  return playlist
 }

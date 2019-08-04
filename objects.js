48ce47fc9dbed15b48d1cc57b116deeb6fc5e8d0
Bailey Wood <woodbmae@gmail.com>
2019-08-04 00:24:28 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,47 +1,6 @@
 
 
-describe('objects', () => {
-  const babelResult = babel.transformFileSync(
-    path.resolve(__dirname, '..', 'objects.js'), {
-      presets: ['es2015']
-    }
-  )
-  jsdom({
-    src: babelResult.code
-  })
-
-  it('defines an object called `playlist` containing at least one artist-song pair', () => {
-    expect(typeof playlist).to.equal('object')
-    expect(Object.keys(playlist).length).to.be.greaterThan(0)
-  })
-
-  describe('updatePlaylist(playlist, artistName, songTitle)', () => {
-    before(() => {
-      playlist['Slowdive'] = 'Alison'
-      playlist['My Bloody Valentine'] = 'Sometimes'
-    })
-
-    it('adds the `artistName: songTitle` key-value pair to `playlist`', () => {
-      updatePlaylist(playlist, 'Phil Ochs', "Here's to the State of Mississippi")
-
-      expect(playlist).
-        to.contain.all.keys({'Slowdive': 'Alison', 'My Bloody Valentine': 'Sometimes', 'Phil Ochs': "Here's to the State of Mississippi"})
-    })
-  })
-
-  describe('removeFromPlaylist(playlist, artistName)', () => {
-    it('removes `artistName` from `playlist`', () => {
-      removeFromPlaylist(playlist, 'Slowdive')
-
-      expect(playlist).
-        to.contain.all.keys({'My Bloody Valentine': 'Sometimes', 'Phil Ochs': "Here's to the State of Mississippi"})
-
-      expect(playlist).
-        not.to.have.all.keys({'Slowdive': 'Alison'})
-    })
-  })
-})
-
+var playlist = { 'Slowdive': 'Alison', 'My Bloody Valentine': 'Sometimes' };
 
 
 function updateplaylist(obj, key, value) {
@@ -49,12 +8,10 @@ function updateplaylist(obj, key, value) {
   return obj;
 }
 
-var playlist = { 'Slowdive': 'Alison', 'My Bloody Valentine': 'Sometimes' };
-
-updateplaylist(playlist, 'Phil Ochs', 'Here`s to the State of Mississippi')
+updateplaylist(playlist, 'Phil Ochs', 'Here`s to the State of Mississippi');
 
 function removeFromPlaylist(playlist, artistName) {
   delete playlist.artistName;
-  return playlist
+  return playlist;
 }
 
